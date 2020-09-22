c8ffbe13406ec18cc5df3d1e396c88bc37c1c0e7
Anusha Bhattacharya <47797609+anusha267@users.noreply.github.com>
2020-09-22 18:47:23 +0530

    21/09

--- app/src/main/java/com/anushabhattacharya/countrynewsapp/MainActivity.java
+++ app/src/main/java/com/anushabhattacharya/countrynewsapp/MainActivity.java
@@ -1,14 +1,94 @@
 package com.anushabhattacharya.countrynewsapp;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.DefaultItemAnimator;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.telephony.TelephonyManager;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.anushabhattacharya.countrynewsapp.api.ApiClient;
+import com.anushabhattacharya.countrynewsapp.api.ApiInterfaces;
+import com.anushabhattacharya.countrynewsapp.models.Articles;
+import com.anushabhattacharya.countrynewsapp.models.News;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Locale;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 public class MainActivity extends AppCompatActivity {
+    public static final String API_KEY="37917dd5f50643038326ff619e98f37d";
+    private RecyclerView recyclerView;
+    private List<Articles> articles=new ArrayList<>();
+    private Adapter adapter;
+    private String TAG=MainActivity.class.getSimpleName();
+    RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        recyclerView=findViewById(R.id.recyclerView);
+        layoutManager = new LinearLayoutManager(MainActivity.this);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setItemAnimator(new DefaultItemAnimator());
+        recyclerView.setNestedScrollingEnabled(false);
+        String countryCode=getCountry();
+        Log.d("countryCode",countryCode);
+        loadArticles(countryCode);
+
+
+    }
+
+
+    public void loadArticles(String countryCode)
+    {
+        ApiInterfaces apiInterfaces= ApiClient.getApiClient().create(ApiInterfaces.class);
+
+        Call<News> call=apiInterfaces.getNews(countryCode,API_KEY);
+
+        call.enqueue(new Callback<News>() {
+            @Override
+            public void onResponse(Call<News> call, Response<News> response) {
+                if(response.isSuccessful() && response.body()!=null)
+                {
+                    if(!articles.isEmpty()){
+                        articles.clear();
+                    }
+                    articles=response.body().getArticle();
+                    adapter=new Adapter(articles,MainActivity.this);
+                    recyclerView.setAdapter(adapter);
+                    adapter.notifyDataSetChanged();
+                }
+                else
+                {
+                    Toast.makeText(MainActivity.this, "Failed to load data!", Toast.LENGTH_SHORT).show();
+                }
+            }
+
+            @Override
+            public void onFailure(Call<News> call, Throwable t) {
+                Toast.makeText(MainActivity.this, "Failed to load news! Check your internet", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+    }
+
+    public String getCountry()
+    {
+        //String locale = this.getResources().getConfiguration().locale.getCountry();
+        //return locale.toLowerCase();
+        TelephonyManager tm = (TelephonyManager)getSystemService(TELEPHONY_SERVICE);
+        String countryCode = tm.getNetworkCountryIso();
+        return countryCode;
     }
 }
--- app/src/main/java/com/anushabhattacharya/countrynewsapp/api/ApiClient.java
+++ app/src/main/java/com/anushabhattacharya/countrynewsapp/api/ApiClient.java
@@ -12,7 +12,19 @@ import javax.net.ssl.X509TrustManager;
 
 import okhttp3.OkHttpClient;
 import retrofit2.Retrofit;
-import retrofit2.Retrofit.Builder;
+import retrofit2.converter.gson.GsonConverterFactory;
+
+import java.security.cert.CertificateException;
+
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.SSLSession;
+import javax.net.ssl.SSLSocketFactory;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+
+import okhttp3.OkHttpClient;
+import retrofit2.Retrofit;
 import retrofit2.converter.gson.GsonConverterFactory;
 
 public class ApiClient {
@@ -20,61 +32,59 @@ public class ApiClient {
     public static final String BASE_URL = "https://newsapi.org/v2/";
     public static Retrofit retrofit;
 
-    public static Retrofit getapiClient()
-    {
-        if(retrofit==null)
-        {
-            retrofit=new Retrofit.Builder().baseUrl(BASE_URL).addConverterFactory(GsonConverterFactory.create()).client(getOkHttpClient().build()).build();
+    public static Retrofit getApiClient(){
+
+        if (retrofit == null){
+
+            retrofit = new Retrofit.Builder().baseUrl(BASE_URL)
+                    .client(getUnsafeOkHttpClient().build())
+                    .addConverterFactory(GsonConverterFactory.create())
+                    .build();
+
         }
 
         return retrofit;
     }
 
-    private static OkHttpClient.Builder getOkHttpClient() {
-
+    public static OkHttpClient.Builder getUnsafeOkHttpClient(){
         try {
-
-            final TrustManager[] allCerts=new TrustManager[]{
+            // Create a trust manager that does not validate certificate chains
+            final TrustManager[] trustAllCerts = new TrustManager[]{
                     new X509TrustManager() {
                         @Override
-                        public void checkClientTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-
+                        public void checkClientTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
                         }
 
                         @Override
-                        public void checkServerTrusted(X509Certificate[] x509Certificates, String s) throws CertificateException {
-
+                        public void checkServerTrusted(java.security.cert.X509Certificate[] chain, String authType) throws CertificateException {
                         }
 
                         @Override
-                        public X509Certificate[] getAcceptedIssuers() {
+                        public java.security.cert.X509Certificate[] getAcceptedIssuers() {
                             return new java.security.cert.X509Certificate[]{};
                         }
                     }
             };
 
-            final SSLContext sslContext= SSLContext.getInstance("SSL");
-            sslContext.init(null,allCerts,new java.security.SecureRandom());
+            // Install the all-trusting trust manager
+            final SSLContext sslContext = SSLContext.getInstance("SSL");
+            sslContext.init(null, trustAllCerts, new java.security.SecureRandom());
 
-            final SSLSocketFactory sslSocketFactory=sslContext.getSocketFactory();
+            // Create an ssl socket factory with our all-trusting manager
+            final SSLSocketFactory sslSocketFactory = sslContext.getSocketFactory();
 
             OkHttpClient.Builder builder = new OkHttpClient.Builder();
-            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) allCerts[0]);
+            builder.sslSocketFactory(sslSocketFactory, (X509TrustManager) trustAllCerts[0]);
             builder.hostnameVerifier(new HostnameVerifier() {
                 @Override
-                public boolean verify(String s, SSLSession sslSession) {
+                public boolean verify(String hostname, SSLSession session) {
                     return true;
                 }
             });
             return builder;
-
-        }
-        catch (Exception e)
-        {
+        } catch (Exception e) {
             throw new RuntimeException(e);
         }
     }
-
-
 
 }
